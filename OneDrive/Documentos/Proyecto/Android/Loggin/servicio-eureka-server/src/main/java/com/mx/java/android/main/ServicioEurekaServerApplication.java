4906125c94c789b495c58d7ffcbb5ec1f7d6eef3
hector0214 <hectoratonal07@gmail.com>
2020-07-01 01:17:57 -0500

    Inicial2

---
+++ OneDrive/Documentos/Proyecto/Android/Loggin/servicio-eureka-server/src/main/java/com/mx/java/android/main/ServicioEurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.mx.java.android.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class ServicioEurekaServerApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(ServicioEurekaServerApplication.class, args);
+		
+	}
+
+}
---
+++ OneDrive/Documentos/Proyecto/Android/Loggin/servicio-eureka-server/src/test/java/com/mx/java/android/main/ServicioEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mx.java.android.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
